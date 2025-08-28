601e24a73b70f8924619290882b0cf9554982b56
Nicholas Coda <nickcodachave@gmail.com>
2025-08-28 18:59:29 -0300

    Corrigido erro de ObjectId e implementada a conexão com o MongoDB

---
+++ src/main/java/com/suplementos/erp/repository/MongoConnection.java
@@ -0,0 +1,18 @@
+package com.suplementos.erp.repository;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoConnection {
+
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static MongoClient mongoClient;
+
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            mongoClient = MongoClients.create(CONNECTION_STRING);
+        }
+        return mongoClient.getDatabase("lojaSuplementos");
+    }
+}
